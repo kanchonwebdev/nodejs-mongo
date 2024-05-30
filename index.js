e3fc7e8192185dc1e1d56de9fb6af702cda03bc4
kanchonwebdev <88568490+kanchonwebdev@users.noreply.github.com>
2024-05-30 23:17:10 +0600

    update

---
+++ index.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb');
+const url = "mongodb://localhost:27017";
+const dbName = "mydb";
+
+const client = new MongoClient(url);
+
+client.connect()
+    .then(() => {
+        const db = client.db(dbName);
+        //return db.createCollection('chat');
+        var myobj = { name: "Company Inc", address: "Highway 37" };
+        return db.collection("chat").insertOne(myobj)
+    })
+    .then(() => {
+        console.log('Collection "chat" created');
+        client.close();
+    })
+    .catch(err => {
+        console.error("Failed to connect to the database. Error:", err);
+    });
